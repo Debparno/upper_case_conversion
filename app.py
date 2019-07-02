a19f67249e6c2fd4826c4ef26b70605992d92282
Debparno <41444610+Debparno@users.noreply.github.com>
2019-07-02 10:44:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['POST'])
+def my_form_post():
+    text = request.form['u']
+    processed_text = text.upper()
+    return processed_text
+
 @app.route('/')
 def my_form():
     return render_template('index.html')
 
-@app.route('/', method = ['post'])
-def my_form_post():
-    text = request.form['u']
-    processed_text = text.upper()
-
-    return processed_text
-
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     app.run()
